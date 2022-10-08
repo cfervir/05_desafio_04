6e47dc010b94deb2356297e57dad035a5e2007f8
Carlos Fernández <carlos@bigbadlion.com>
2022-10-08 02:33:22 -0300

    Modified some styles and modified the cart view!

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -3,12 +3,23 @@ import { useNavigate } from "react-router-dom";
 
 import PizzaContext from "../PizzaContext";
 import Images from "../images";
-import { locale } from "../helpers";
+import { locale, capitalize } from "../helpers";
 
 export default function Cart() {
 
   const { total, pizzaData, addItem, removeItem } = useContext(PizzaContext);
   const goBack = useNavigate();
+
+  const listIngredients = (ing) => {
+    const list = ing.ingredients.map((data, i, {length} ) => {
+      if (length - 1 !== i ) {
+        return `${capitalize(data)}, `
+      } else {
+        return `${capitalize(data)}`;
+      }
+    });
+    return list;
+  };
 
   if (total.length === 0) {
     return (
@@ -23,21 +34,40 @@ export default function Cart() {
 
   return (
     <div className="container container--cart">
-      <h1>Pagaaaa</h1>
+      <h1>Detalle de tu compra</h1>
+      <div className="cart card">
         { total.map((items, i) => (
-          <div key={i}>
-            <p className="capitalize"> { items.name } </p>
-            <p> ${ locale(items.price) } </p>
-            <p> ${ locale(items.price / items.qty) } </p>
-            { pizzaData.filter((x => x.id === items.id)).map((buttons) => (
-                <button key={ buttons.id } className="btn btn--add" onClick={ () => addItem(buttons) }> + </button>))
-            } <span className="btn--between"> {items.qty} </span>
-            { pizzaData.filter((x => x.id === items.id)).map((buttons) => (
-                <button key={ buttons.id } className="btn btn--remove" onClick={ () => removeItem(buttons) }> - </button>))
-            }
+          <div className="cart__items container--flex" key={i}>
+            <div className="cart__info container--flex">
+              <img src={ items.img } alt={ capitalize(items.name) } className="cart__info--img" />
+              <div className="cart__info--details">
+                <h2 className="capitalize"> { items.name } </h2>
+                <div className="cart__info--list">
+                  <strong>Ingredientes:</strong> { listIngredients(items) }
+                </div>
+                <p className="cart__info--single"> <strong>Precio:</strong> { `$${ locale(items.price / items.qty) }` } </p>
+              </div>
+            </div>
+            <div className="cart__qty container--flex">
+              <div className="cart__qty--total">
+                <h2><span className="cart__qty--total-s">$</span>{ locale(items.price) }</h2>
+              </div>
+              <div className="cart__qty--buttons container--flex">
+                { pizzaData.filter((x => x.id === items.id)).map((buttons) => (
+                    <button key={ buttons.id } className="btn btn--remove" onClick={ () => removeItem(buttons) }> - </button>))
+                }
+                  <span className="btn--between"> {items.qty} </span>
+                { pizzaData.filter((x => x.id === items.id)).map((buttons) => (
+                    <button key={ buttons.id } className="btn btn--add" onClick={ () => addItem(buttons) }> + </button>))
+                }
+              </div>
+            </div>
           </div>
         ))}
-      ${ locale(total.reduce((a, b)=> a + b.price,0)) }
+      </div>
+      <div className="cart__total">
+        { `El total es: $${locale(total.reduce((a, b)=> a + b.price,0))}` }
+      </div>
     </div>
   );
 }
